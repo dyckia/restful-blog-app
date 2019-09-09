3d2422e7a6ecf12aaa39a7a0b4cb53f640065000
Jason-GoormIDE <dyckia69@gmail.com>
2019-09-09 17:22:41 +0000

    setup the environment

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
+
+app.listen(3000, () => {
+	console.log("server is listening on port 3000");
+});
