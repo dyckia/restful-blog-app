a289f1f4bad2f13b4addbdd2ee664cb320877f3c
Jason-GoormIDE <dyckia69@gmail.com>
2019-09-09 17:44:31 +0000

    connect to MongoDB Atlas

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a RESTful blog app.
+
+
+## Todos
+### v0.1
+- [x] setup the npm environment
+- [x] connect to MongoDB Atlas
+- [ ] create blog model 
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const keys = require("./.configure/keys.js");
+const mongoose = require("mongoose");
+
+mongoose.connect(keys.mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 	res.send("hello");
 });
-
 
 app.listen(3000, () => {
 	console.log("server is listening on port 3000");
